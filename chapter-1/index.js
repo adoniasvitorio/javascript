2eb9bb2e27807cee2080fc2eef617831dbda2be6
Adonias Vitorio <adonias.vitorio@gmail.com>
2020-12-18 04:53:03 -0300

    section 1.1 - using console.log()

---
+++ chapter-1/index.js
@@ -0,0 +1,32 @@
+
+// Simple example.
+console.log('helle world')
+
+// Loggin variables.
+var foo = 'hello world'
+console.log(foo)
+
+var thisVar = 'first value'
+var thatVar = 'second value'
+console.log('thisValue: ',thisVar,'and thatValue:',thatVar )
+
+// Placeholdes.
+var greet = 'Hello', who = 'world'
+console.log('%s %s!', greet, who)
+
+// Loggin Objects
+
+console.log({
+    'Email':'adonias.vitorio@gmail.com',
+    'Groups': {},
+    'Id': 33,
+    'IsHiddenInUI': false,
+    'IsSiteAdmin': false,
+    'LoginName': 'i:0#.w|virtualdomain\\user2',
+    'PrincipalType': 1,
+    'Title': 'user1'
+    })
+
+// Loggin HTML elements
+
+console.log(document.body)
