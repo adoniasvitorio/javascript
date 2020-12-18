50bb29e5fe2d719655405a6d5b9e7e679a61f64a
Adonias Vitorio <adonias.vitorio@gmail.com>
2020-12-18 05:36:56 -0300

    section 1.3: using window.alert()

---
+++ chapter-3/index.js
@@ -0,0 +1,11 @@
+// Simple example
+
+window.alert('Hello world')
+
+// Displays a visual alert box on screen.
+
+var message = 'Hello world'
+window.alert(message)
+
+// 'window' is the global object. I can call only 'alert'.
+alert(message)
