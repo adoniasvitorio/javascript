c09932c847f887cbd331abac4e2986f037922211
Adonias Vitorio <adonias.vitorio@gmail.com>
2021-05-17 08:31:08 -0300

    estudo de strings com javascript

--- tipos de dados/index.js
+++ tipos de dados/index.js
@@ -3,10 +3,10 @@
 var string_1 = "eu sou uma string com aspas dupla"
 var string_2 = 'eu sou uma string com aspas simples'
 var string_3 = `eu sou uma string com crases`
-
-
 var string_4 = "eu tenho uma palavra com 'apas simples' dentro da frase"
 var string_5 = 'eu tenho uma palavra com "apas duplas" dentro da frase'
-
 var string_6 = `a soma de 2 + 2 é igual a ${2+2}. Eu tenho uma expressão dentro da frase`
-console.log(string_6)
+
+
+//numeros
+
